3cb992db5b00a6c13d24d37cfe634f3aa97c1edd
Mayank Sachan <otnemio@gmail.com>
2020-07-22 20:54:45 +0530

    Update begin.py

--- begin.py
+++ begin.py
@@ -2,7 +2,7 @@ import subprocess
 import json
 import re
 
-
+json_var_ip={}
 oo="" #variable to store output
 ee="" #variable to store error
 def runCommand(cmd,*argc):
@@ -34,42 +34,47 @@ def runCommand(cmd,*argc):
             ee=reserr.strip()
             break
 
-runCommand('nmap','192.168.43.225-226','-p 80-85')
-print("Output:")
-#print(oo)
+
 #Json
 # Data to be written
 def extract_ip(ip_str):
     return re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_str).group()
 
+for counter in range(1, 45, 5):
+    print(f'nmap for 192.168.43.{counter}-{counter + 4}:')
+    runCommand('nmap', f'192.168.43.{counter}-{counter + 4}', '-p 80,85,22', '-v')
+    print("Output:")
+    #print(oo)
+    print("Error:")
+    print(ee)
 
-json_var_ip={}
+    line=""
+    flagBlank=False
+    for char in oo:
+        if char!='\n':
+            line+=char
+        else:
+            #print("-->"+line)
+            if line.find("Nmap scan report for ")>=0:
+                #print("ip-->" + line)
+                ip_str=line[21:]
+                #print(":::::ip string is "+ ip_str)
+                ip=extract_ip(ip_str)
+                json_var_ip[ip]= {}
+            if (len(line) < 1) and flagBlank == False:
+                #print("blank-->" + line + str(flagBlank))
+                flagBlank = True
+            elif (len(line) < 1) and flagBlank == True:
+                #print("blank-->" + line + str(flagBlank))
+                flagBlank = False
+            if flagBlank is True and len(line)>0 and line[0].isdigit():
+                #print(":::::::"+line)
+                json_var_ip[ip][line.split(' ')[0]]=[line.split(' ')[1],line.split(' ')[2]]
+            line=""
 
-line=""
-flagBlank=False
-for char in oo:
-    if char!='\n':
-        line+=char
-    else:
-        #print("-->"+line)
-        if line.find("Nmap scan report for ")>=0:
-            #print("ip-->" + line)
-            ip_str=line[21:]
-            print("ip string is "+ ip_str)
-            ip=extract_ip(ip_str)
-            json_var_ip[ip]= {}
-        if (len(line) < 1) and flagBlank == False:
-            #print("blank-->" + line + str(flagBlank))
-            flagBlank = True
-        elif (len(line) < 1) and flagBlank == True:
-            #print("blank-->" + line + str(flagBlank))
-            flagBlank = False
-        if flagBlank is True and len(line)>0 and line[0].isdigit():
-            print(":::::::"+line)
-            json_var_ip[ip][line.split(' ')[0]]=[line.split(' ')[1],line.split(' ')[2]]
-        line=""
-flagStart=False
-flagPort=False
+
+    flagStart=False
+    flagPort=False
 
 # Serializing json
 json_object = json.dumps(json_var_ip, indent=4)
@@ -80,8 +85,11 @@ with open("sample.json", "w") as outfile:
     outfile.write(json_object)
 print(json_object)
 
-print("Error:")
-print(ee)
+#runCommand('nmap','127.0.0.1','-O')
+#runCommand('nmap','127.0.0.1','-F')
+#runCommand('nmap','192.168.43.0/24','-sP')
+
+#runCommand('nmap','192.168.43.221-225','-p 80-85', '-v')
 #runCommand('nmap','127.0.0.1','-O')
 #runCommand('nmap','127.0.0.1','-F')
 #runCommand('nmap','192.168.43.0/24','-sP')
