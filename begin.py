4d2b3d6c4c21bdf00fab57fd17dd51be0fea66b5
Mayank Sachan <otnemio@gmail.com>
2020-07-22 11:14:23 +0530

    Update begin.py

--- begin.py
+++ begin.py
@@ -1,13 +1,14 @@
-import subprocess 
+import subprocess
+import json
 
 oo="" #variable to store output
 ee="" #variable to store error
-def runCommand(cmd,arg1,arg2):
+def runCommand(cmd,*argc):
     global oo
     global ee
     resout=""
     reserr=""
-    process = subprocess.Popen([cmd,arg1,arg2], 
+    process = subprocess.Popen([cmd,argc[0],argc[1],argc[2]],
                            stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                            universal_newlines=True)
@@ -22,7 +23,7 @@ def runCommand(cmd,arg1,arg2):
         return_code = process.poll()
         if return_code is not None:
             print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output 
+            # Process has finished, read rest of the output
             for output in process.stdout.readlines():
                 resout = resout+"\n"+str(output.strip())
             for error in process.stderr.readlines():
@@ -31,13 +32,33 @@ def runCommand(cmd,arg1,arg2):
             ee=reserr.strip()
             break
 
-runCommand('nmap','192.168.43.225-230','-p 10-30')
+runCommand('nmap','192.168.43.225-226','-p 80-85','-v')
 print("Output:")
-print(oo)
+#print(oo)
+line=""
+flagBlank=False
+for char in oo:
+    if char!='\n':
+        line+=char
+    else:
+        #print("-->"+line)
+        if line.find("Nmap scan report for ")>=0:
+            print("ip-->" + line)
+            print("ip is "+line[21:])
+        if (len(line) < 1) and flagBlank == False:
+            print("blank-->" + line + str(flagBlank))
+            flagBlank = True
+        elif (len(line) < 1) and flagBlank == True:
+            print("blank-->" + line + str(flagBlank))
+            flagBlank = False
+        if flagBlank is True:
+            print(":::::::"+line)
+        line=""
+flagStart=False
+flagPort=False
+
 print("Error:")
 print(ee)
 #runCommand('nmap','127.0.0.1','-O')
 #runCommand('nmap','127.0.0.1','-F')
 #runCommand('nmap','192.168.43.0/24','-sP')
-
-
