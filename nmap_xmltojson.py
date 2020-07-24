f550c2f691439704a668f400fa3a63d2433ed407
Mayank Sachan <otnemio@gmail.com>
2020-07-24 15:04:37 +0530

    Update nmap_xmltojson.py

--- nmap_xmltojson.py
+++ nmap_xmltojson.py
@@ -17,21 +17,19 @@ def runcommand(cmd, *argc):
         output = process.stdout.readline()
         #Only print the output lines necessary
         if len(output.strip()) > 0:
-            if output.find("Ping Scan Timing: About") >= 0: # extract percent for Ping Scan progress
-                start = output.find("About") + 6  # starting location of percent done
-                end = output.find("done") - 1  # ending location of percent done
-                print(f"Ping::{output.strip()[start:end]}::") # percent done
-            elif output.find("Stealth Scan Timing: About") >= 0: # extract percent for Stealth Scan progress
-                start = output.find("About") + 6  # starting location of percent done
-                end = output.find("done") - 1  # ending location of percent done
-                print(f"Stealth::{output.strip()[start:end]}::") # percent done
-        return_code = process.poll() # fetch return code from Process
+            if output.find("Ping Scan Timing: About") >= 0:         # extract percent for Ping Scan progress
+                start = output.find("About") + 6                    # starting location of percent done
+                end = output.find("done") - 1                       # ending location of percent done
+                print(f"Ping::{output.strip()[start:end]}::")       # percent done
+            elif output.find("Stealth Scan Timing: About") >= 0:    # extract percent for Stealth Scan progress
+                start = output.find("About") + 6                    # starting location of percent done
+                end = output.find("done") - 1                       # ending location of percent done
+                print(f"Stealth::{output.strip()[start:end]}::")    # percent done
+        return_code = process.poll()                                # fetch return code from Process
         if return_code is not None:
             print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output,
-            # nothing remains to be read from stdout so commented these lines
-            # for output in process.stdout.readlines():
-            #    print(output.strip())
+            # for output in process.stdout.readlines():             # Process has finished, read rest of the output,
+            #    print(output.strip())                              # nothing remains to be read from stdout so commented these lines
             break
 
 
@@ -41,11 +39,11 @@ runcommand('nmap', f'192.168.43.1-60 -p 22,23,80 --stats-every 2s --oX nmap_outp
 f = open("nmap_output.xml")
 xml_content = f.read()
 f.close()
-json_object2 = json.dumps(xmltodict.parse(xml_content) , indent=4, sort_keys=True)
+json_object = json.dumps(xmltodict.parse(xml_content) , indent=4, sort_keys=True)
 
 
 # Writing to sample.json
 print("Json Output:\n")
 with open("sample.json", "w") as outfile:
-    outfile.write(json_object2)
-print(json_object2)
+    outfile.write(json_object)
+print(json_object)
